95b7d60eb0f17c5d7fc1a07360c929476b15a216
gbzhu <wiki.zhu@sap.com>
2018-07-25 13:43:12 +0800

    context json

--- crawler_src/first_crawler.py
+++ crawler_src/first_crawler.py
@@ -1,37 +1,32 @@
 import configparser
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import time
+from urllib import request
+from urllib.parse import quote
+import json
 
-config = configparser.ConfigParser()
-config.read(filenames='../config.ini', encoding='utf8')
-# set browser
-browserPath = str(config['system']['browserPath'])
-dirver = webdriver.PhantomJS(executable_path=browserPath)
-# assign parser
-parser = "html5lib"
+
+# config = configparser.ConfigParser()
+# config.read(filenames='../config.ini', encoding='utf8')
+# # set browser
+# browserPath = str(config['system']['browserPath'])
+# dirver = webdriver.PhantomJS(executable_path=browserPath)
+# # assign parser
+# parser = "html5lib"
 
 
 def first_crawler(url: str):
     print(url)
     # 访问目标网页地址
-    dirver.get(url)
-    time.sleep(100)
-    a_more = dirver.find_elements(By.CLASS_NAME, 'more')[0]
-    flag = True
-    while flag:
-        if a_more.text == '加载更多':
-            a_more.click()
-        else:
-            flag = False
-    # 解析目标网页的 Html 源码
-    bs_obj = BeautifulSoup(dirver.page_source, parser)
-    a_more = bs_obj.find(name='a', class_='more', href='javascript:;')
+    response = request.urlopen(url)
+    context = response.read().decode('utf-8')
+    json_obj = json.loads(context)
+    pass
 
 
 if __name__ == '__main__':
-    movies_type = str(config['douban']['type'])
-    for t in movies_type.split('&'):
-        type_url = 'https://movie.douban.com/explore#!type=movie&tag=' + t + '&sort=recommend&page_limit=20&page_start=0'
-        first_crawler(url=type_url)
+    first_crawler(
+        'https://movie.douban.com/j/search_subjects?type=movie&tag=%E5%8A%A8%E4%BD%9C&sort=recommend&page_limit=20&page_start=240')
+    # movies_type = str(config['douban']['type'])
+    # for t in movies_type.split('&'):
+    #     type_url = 'https://movie.douban.com/j/search_subjects?type=movie&tag=' + quote(
+    #         t) + '&sort=recommend&page_limit=20&page_start=0'
+    #     first_crawler(url=type_url)
